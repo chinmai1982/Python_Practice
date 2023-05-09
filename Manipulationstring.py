deb9ba8a8678d473e50176656f0ee5a45df3d558
chinmai1982 <chinmaisutar@gmail.com>
2023-05-09 00:22:32 -0500

    My first commit

---
+++ Manipulationstring.py
@@ -0,0 +1,8 @@
+#String Manipulation
+name = input("Enter your Name Here : ")
+print("Your Name is :" + name)
+age = int(input("Enter your Age Here:"))
+print(age)
+print("Door" + "Closed")
+print(4 * "Apple")
+print("6" + "Oranges")
