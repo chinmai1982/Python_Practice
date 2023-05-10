acbc14c76efbc62176d9711ec6bea45fb31f9233
chinmai1982 <chinmaisutar@gmail.com>
2023-05-10 12:27:31 -0500

    modified list file

--- Example_list.py
+++ Example_list.py
@@ -56,5 +56,29 @@ numdivisors = []
 for num in list(range(1,num1+1)):
     if num1 % num == 0 :
         numdivisors.append(num)
-print(numdivisors)        
+print(numdivisors) 
+print("    ")
 
+'''
+Take two lists, say for example these two:
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates).
+Make sure your program works on two lists of different sizes.
+'''
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+nlist = []
+
+print(a)
+print(b)
+
+print("    ")
+
+for item in a:
+    if item in b:
+        if item not in nlist :
+            nlist.append(item)
+
+print(nlist)
