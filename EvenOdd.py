e0d9377e71326af4ec93f11161388994b1f2e890
chinmai1982 <chinmaisutar@gmail.com>
2023-05-10 00:01:46 -0500

    commit new file

---
+++ EvenOdd.py
@@ -0,0 +1,12 @@
+'''Concepts :
+Modular arithmetic (the modulus operator)
+Conditionals (if statements)
+Checking equality'''
+
+num1 = int(input("Enter the number here : "))
+num2 = 2
+if((num1 % num2) == 0):
+    print("this is an even number")
+else:
+    print("This is an odd number")
+    
