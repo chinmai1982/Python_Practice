5a2b85371f10362e406b2644905c20880daadb24
chinmai1982 <chinmaisutar@gmail.com>
2023-05-15 16:19:40 -0500

    ExRandomModule.py

---
+++ ExRandomModule.py
@@ -0,0 +1,13 @@
+'''Random Numbers (and Modules)
+This is using Python code that somebody else wrote. 
+In Python, these formally-distributed code packages are called modules.
+The thing we want from a module in this exercise is the ability to generate random numbers. 
+This comes from the random module'''
+
+#Generate a random number between 1 and 9.
+
+import random
+
+num = random.randint(1,9)
+
+print(num)
