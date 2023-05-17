89f1a9720729759c40e8796120f380e1240962e3
chinmai1982 <chinmaisutar@gmail.com>
2023-05-16 22:59:10 -0500

    my first commit

---
+++ Ex_Tuple.py
@@ -0,0 +1,9 @@
+'''
+Tuple is an order collection of elements enclosed within ()
+Tuples are immutable
+tuple contains elements with different datatypes
+'''
+
+tup =('Chinmai',33,True)
+print(tup)
+print(type(tup))
