8c4625130db8d5c546d935f0cc6ba19fc54a5388
chinmai1982 <chinmaisutar@gmail.com>
2023-05-22 12:06:35 -0500

    evenodd file modified for jenkins

--- EvenOdd.py
+++ EvenOdd.py
@@ -3,7 +3,8 @@ Modular arithmetic (the modulus operator)
 Conditionals (if statements)
 Checking equality'''
 
-num1 = int(input("Enter the number here : "))
+#num1 = int(input("Enter the number here : "))
+num1 = 9
 num2 = 2
 if((num1 % num2) == 0):
     print("this is an even number")
