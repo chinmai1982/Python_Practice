8aca1892308dc32880450c84ae7bb5fb7f884411
chinmai1982 <chinmaisutar@gmail.com>
2023-05-26 13:14:31 -0500

    editing...

--- Ex_Dictionary.py
+++ Ex_Dictionary.py
@@ -11,4 +11,10 @@ fruits['apple']=100
 fruits['Mango']=70
 print(fruits.items())
 
+fruits1 = {'apple':30,'banana':40,'Orange':50,'Grapes':60}
+fruits2 = {'guava':90,'cherry':22,}
+fruits1.update(fruits2)
+print(fruits1.items())
+
+
 
