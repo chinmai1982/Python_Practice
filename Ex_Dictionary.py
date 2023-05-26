0e0cbb7c4ce5e2f9307df4b46c541f204d368adf
chinmai1982 <chinmaisutar@gmail.com>
2023-05-26 13:04:46 -0500

    commiting dictionary file

---
+++ Ex_Dictionary.py
@@ -0,0 +1,11 @@
+'''
+Dictionary is an order collection of key-value pairs enclosed within {}
+Dictionary is mutable
+'''
+fruits = {'apple':30,'banana':40,'Orange':50,'Grapes':60}
+print(fruits.keys())
+print(fruits.values())
+print(fruits.items())
+fruits['apple']=100
+print(fruits.items())
+
